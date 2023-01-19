973e7d20a143557e693d00883dcf98366032bc87
PaniaguaF <98367564+PaniaguaF@users.noreply.github.com>
2023-01-18 19:20:48 -0600

    Update Fibonnaci.java
    New features

--- Fibonnaci.java
+++ Fibonnaci.java
@@ -5,18 +5,18 @@
  */
 package calidad_y_pruebas;
 //Hecho por Salvador Paniagua individualmente. :(
-class Fibonnaci{  
+class fibo{  
 
 	public static void main(String[] args) {
 
-		int max = 100000000; //Numeros de la secuencia fibonacci que equeremos
-		int numero1 = 0, numero2 = 1; //Numeros que empezaran la secuencia
+		int max = 100; //Numeros de la secuencia fibonacci que equeremos
+
+		int numero1 = 0, numero2 = -1; //Numeros que empezaran la secuencia
+                //Update: Se puede elegir entre numeros negativos y positivos
         
 		for (int i=1; numero1 < max; i++) {
-			System.out.print(numero1 + "  ");
-                
-                        
-			int n = numero1 + numero2;
+			System.out.print(numero1 + " / ");
+                        int n = numero1 + numero2;
 			numero1 = numero2;
 			numero2 = n;
                 }
